229d98755e65fba00bf1683eaf59c920b34c64c8
enmotion <40427163@qq.com>
2023-07-24 17:19:35 +0800

    创建，删除页面或组件命令完成

--- bin/lib/index.js
+++ bin/lib/index.js
@@ -17,7 +17,35 @@ const handleError = err => {
   process.exit(1);
 };
 
+function createFolder(folderPath) {
+  fsExtra.ensureDir(folderPath, (err) => {
+    if (err) {
+      throw Error(`error:${err}`)
+    } else {
+      console.log(`created:${folderPath}`);
+    }
+  });
+}
+
+function removeFolder(folderPath) {
+  fsExtra.pathExists(folderPath,(err,exists)=>{
+    if(exists){
+      fsExtra.remove(folderPath)
+      .then(() => {
+        // console.log(`remove:${folderPath}`);
+      })
+      .catch((err) => {
+        throw Error(`error:${err}`)
+      });
+    }else{
+      throw Error(`error: No ${folderPath} file detected from the current location.`)
+    }
+  });
+}
+
 module.exports={
   replaceContentToFile,
-  handleError
+  handleError,
+  createFolder,
+  removeFolder
 }
